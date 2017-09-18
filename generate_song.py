67ffef8bf239249fd48c076f28cb4cecf64036f4
Justin Kunimune <justinkunimune@gmail.com>
2017-09-18 17:48:42 -0400

    colon colon colon colon colon colon colon colon
    Schommer taught me how to make better Markov chains, and wow is this great. I also greatly improved the handling of line breaks, line lengths, and data storage, and I fixed via workaround the issue where it sometimes reverts to speaking in text rather than phonemes.

--- generate_song.py
+++ generate_song.py
@@ -1,63 +1,111 @@
 #generate_song.py
 
 from collections import defaultdict
+import math
+import numpy as np
+import os
 import pickle
 import random as rng
 import re
 import subprocess
-from urllib import request, error
-
-ORDER = 4
-pronounciation_guide = dict()
+import tempfile
+import wave
 
 
-def to_kirshenbaum(line):
-	proc = subprocess.run(['espeak','-v','en-us','-q','-x',line], encoding='ascii',stdout=subprocess.PIPE)
-	return proc.stdout
+MEMORY_DECAY = .3 #the last last phoneme has about this as much effect as the last phoneme
+MIN_ORDER = 2
+MAX_ORDER = int(math.log(.01, MEMORY_DECAY)) #automatically stop memorising when the effect becomes negligible
+PREF_LENGTH = 30
+
+CHARACTER = list("\n _-,':?lOmEStak@#gnIeUopujidrTsVwZAhfbDNLvz0123")
+INDEX = {ph:i for i,ph in enumerate(CHARACTER)}
+
+
+def learn_english():
+	transitions = [] #the list of transition dicts for each order up to the length of the input
+	for i in range(MAX_ORDER):
+		transitions.append(defaultdict(lambda:np.zeros(len(CHARACTER))))
+
+	with open('input.txt','r') as f:
+		inline = re.sub(r'\r\n+', ' ', f.read())
+		proc = subprocess.run(['espeak','-v','en-us','-q','-x',inline], encoding='ascii', stdout=subprocess.PIPE)
+		source = re.sub(r'[;!]', '', re.sub(r'\n ', '\n', proc.stdout)) #I've no idea from whence these ;s and !s come, but they ruin everything.
+		print('"""')
+		print(source)
+		print('"""')
+		for i in range(len(source)):
+			for j in range(1,min(MAX_ORDER,i)): #skip the zeroth order transition matrix, because we will always start with at least a newline
+				transitions[j][source[1-i:1-i+j]][INDEX[source[-i]]] += 1
+
+	for i, m in enumerate(transitions):
+		transitions[i] = dict(m)
+
+	with open('data.pic', 'wb') as f:
+		pickle.dump(transitions, f)
+
+	return transitions
+
+
+def markov_chain(seed, transitions, length=float('inf'), force_length=True):
+	line = seed
+	while True:
+		vector = np.zeros(len(CHARACTER))
+		for i in range(min(len(line), MIN_ORDER), min(len(line)+1, MAX_ORDER)):
+			if line[:i] in transitions[i]:
+				vector = vector + (MEMORY_DECAY**i) * (transitions[i][line[:i]]/transitions[i][line[:i]].sum())
+			else:
+				break
+		if len(line) >= length and vector[INDEX['\n']]/vector.sum() > .01:
+			break #exit if we are at capacity and a newline next is plausible
+		if force_length:
+			vector[INDEX['\n']] = 0 #there can be no newline if the length is fixed
+		if vector.sum() == 0:
+			break #exit if there is literally no possible next step (unlikely)
+		else:
+			line = np.random.choice(CHARACTER, p=vector/vector.sum()) + line
+		if line[0] == '\n':
+			line = line[1:] #line-breaks signal the end of the thing
+			break
+	return line
 
 
 if __name__ == '__main__':
 
-	transition = defaultdict(lambda:[])
-	chars = ' '*ORDER
-	line_endings = []
-	with open('input.txt','r') as f:
-		phonetics = to_kirshenbaum(f.read())
-		phonetics = re.sub(r'\n+', '\n', phonetics)
-		for char in reversed(phonetics):
-			transition[chars].append(char)
-			if chars[-1] == '\n':
-				line_endings.append((char+chars).split('\n')[-2])
-			chars = char+chars[:-1]
+	fd, temp = tempfile.mkstemp()
 
-	song = ""
-	for i in range(10):
+	try:
+		with open('data.pic', 'rb') as f:
+			transitions = pickle.load(f)
+	except IOError:
+		transitions = learn_english()
+
+	text = ""
+	speech = []
+	for i in range(8):
 		lines = []
+		lines.append(markov_chain("\n", transitions, length=PREF_LENGTH, force_length=False))
+		lines.append(markov_chain("\n", transitions, length=len(lines[0])))
 		for j in range(2):
-			line = rng.choice(line_endings)
-			while True:
-				if line[:ORDER] in transition:
-					line = rng.choice(transition[line[:ORDER]]) + line
-					if line[0] == '\n':
-						line = line[1:]
-						break
-				else:
-					break
-			lines.append(line)
+			seed = lines[j][lines[j].rfind("'"):] if "'" in lines[j] else "\n"
+			lines.append(markov_chain(seed, transitions, length=len(lines[j])))
 
-		for j in range(2):
-			line = max(lines[j][max(0,lines[j].rfind("'")):], lines[j][-ORDER:], key=len)
-			while len(line.replace('\n','')) < len(lines[j]):
-				if line[:ORDER] in transition:
-					line = rng.choice(transition[line[:ORDER]]) + line
-				else:
-					print("{} was not in the transition matrix!!!".format(repr(line[:ORDER])))
-					break
-			lines.append(line.replace('\n',''))
+		q_text = "".join([line.strip()+"_:_:_:\n" for line in lines])
+		proc = subprocess.run(['espeak','-v','en-us','-s','120','-w',temp,'[['+q_text+']]'], stdout=subprocess.PIPE)
+		w = wave.open(temp, 'rb')
 
-		song += "_:_:_:\n".join(lines)+"_:_:_:\n"
+		print(q_text, end='')
+		text += q_text
+		speech.append((w.getparams(), w.readframes(w.getnframes())))
+		w.close()
 
-	print(song)
+	os.close(fd)
+	os.remove(temp)
+
 	with open('output.txt', 'w') as f:
-		f.write(song)
-	subprocess.call(['espeak','-v','en-us','-s','100','[['+song+']]'])
+		f.write(text)
+
+	outf = wave.open('output.wav', 'wb')
+	outf.setparams(speech[0][0])
+	for params, frames in speech:
+		outf.writeframes(frames)
+	outf.close()
